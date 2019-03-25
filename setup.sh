1fee6e9bfc4f8741d656f74bcb428330b21681ab
Steven Chen <steven@alphash.io>
2019-03-25 23:50:01 +0800

    refactor setup.sh

--- setup.sh
+++ setup.sh
@@ -3,29 +3,30 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-checked="${GREEN}√${NC} "
+CHECKED_FLAG="${GREEN}[OK]${NC} "
 
-# Install `xcode` command line tool first
+# Install `xcode` command line and development tools
 install_xcode() {
-  if [ ! package_loc="$(xcode-select -p)" ] || [ -z "${package_loc}" ]; then
+  if ! xcode_path="$(xcode-select -p)" || [ -z "${xcode_path}" ]; then
     echo "Installing Xcode Command Line Tools"
     xcode-select --install # git and other development tools will be installed
-    git --version # verify installation and accept license terms
+    git --version          # verify installation and accept license terms
   else
-    printf "${checked}Xcode developer tools have been installed, skip install xcode\n"
+    printf "${CHECKED_FLAG}Xcode developer tools have been installed, skip installing xcode\n"
   fi
 }
 
+# Install command line tool
 install_package() {
-  local PACKAGE=$1
-  local NAME=$2
-  local CMD=$3
+  local cmd=$1
+  local name=$2
+  local install_cmd=$3
 
-  if [ ! package_loc="$(type -p "${PACKAGE}")" ] || [ -z "${package_loc}" ]; then
-    echo "Installing '${NAME}'"
-    eval ${CMD}
+  if ! installed_path="$(type -p "${cmd}")" || [ -z "${installed_path}" ]; then
+    echo "Installing '${name}'"
+    eval ${install_cmd}
   else
-    printf "${checked}${NAME} has been installed, '`which ${PACKAGE}`', skip installing ${NAME}\n"
+    printf "${CHECKED_FLAG}${name} has been installed, '`which ${cmd}`', skip installing ${name}\n"
   fi
 }
 
@@ -33,67 +34,83 @@ install_homebrew() {
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 }
 
-# Install `vim` as OSX's default one ships with `-clipboard`, but we need `+clipboard` to enable
-# copy to clipboard across terminals
-install_vim() {
-  brew install vim
-  # Config git to use customized vim rather than the default `vim`
-  git config --global core.editor /usr/local/bin/vim
-}
-
-backup_file() {
-  local path=$1
-  if [ -f "${path}" ]; then
-    mv ${path} ${path}.bak
-  fi
-}
-
-# Config bash preferences
-configure_bash() {
-  bash_conf="$HOME/.bash_profile"
-  bash_conf_local="$HOME/.bash_aliases"
-  backup_file "${bash_conf}" && cp ./bash/bash_profile ${bash_conf}
-  backup_file "${bash_conf_local}" && cp ./bash/bash_profile.local ${bash_conf_local}
-}
-
-configure_checkstyle() {
-  checkstyle_d="/usr/local/etc/checkstyle.d"
-  backup_file "${checkstyle_d}"
-  cp -r checkstyle.d ${checkstyle_d}
-
-  java_checkstyle_script="/usr/local/bin/java-checkstyle"
-  backup_file "${java_checkstyle_script}"
-  cp ./scripts/java-checkstyle ${java_checkstyle_script}
-
-  style_wrapper_dir="/usr/local/opt/style"
-  backup_file "${style_wrapper_dir}"
-  cp -r ./style ${style_wrapper_dir}
-
-  # Create soft links for wrapper scripts
-  ln -f -s ${style_wrapper_dir}/bin/cppstyle /usr/local/bin/cppstyle
-  ln -f -s ${style_wrapper_dir}/bin/google-cpp-style /usr/local/bin/google-cpp-style
-}
-
 # Config `vim`
 configure_vim() {
-  vimrc_conf="$HOME/.vimrc"
-  vimrc_local="$HOME/.vimrc.local"
-  vimrc_bundles="$HOME/.vimrc.bundles"
+  local vimrc_conf="$HOME/.vimrc"
+  local vimrc_local="$HOME/.vimrc.local"
+  local vimrc_bundles="$HOME/.vimrc.bundles"
+
   backup_file "${vimrc_conf}"    && cp ./vim/vimrc ${vimrc_conf}
   backup_file "${vimrc_local}"   && cp ./vim/vimrc.local ${vimrc_local}
   backup_file "${vimrc_bundles}" && cp ./vim/vimrc.bundles ${vimrc_bundles}
-  clang_release_date=`ls -t /usr/local/Cellar/clang-format | awk 'NR==1{print $1}'`
+  local clang_release_date=`ls -t /usr/local/Cellar/clang-format | awk 'NR==1{print $1}'`
   sed -i '' -e 's/__CHANGEME__/'"${clang_release_date}"'/g' ${vimrc_local} # no backup before replacement
 }
 
 # Install `vim` plugins
 install_vim_plugins() {
-  vim -c PluginInstall -c q -c q
+  local vim_d="/usr/local/etc/vim.d"
+  local ycm_extra_conf="$HOME/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py"
+
+  /usr/local/bin/vim -c PluginInstall -c q -c q
   $HOME/.vim/bundle/YouCompleteMe/install.py --clang-completer # `cmake` is required
-  vim_d="/usr/local/etc/vim.d"
-  ycm_extra_conf="$HOME/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py"
   mkdir -p ${vim_d} && cp ${ycm_extra_conf} ${vim_d}/
 }
+
+# Install `vim` as OSX's default one ships with `-clipboard` by using Homebrew, but we need
+# `+clipboard` to enable copy to clipboard across terminals
+install_and_configure_vim() {
+  brew install vim
+  # Config git to use customized vim rather than the default `vim`
+  git config --global core.editor /usr/local/bin/vim
+
+  # Install vundle
+  # git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+  install_package vundle vundle "git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim"
+
+  configure_vim       # configure preferences and add vundle plugins
+  install_vim_plugins # Depends on vundle
+}
+
+backup_file() {
+  local file_path=$1
+
+  if [ -f "${file_path}" ]; then
+    if [ -f "${file_path}.bak" ]; then
+      local ts=$(date '+%Y%M%d%H%m%S')
+      mv "${file_path}" "${file_path}.bak.${ts}"
+    else
+      mv "${file_path}" "${file_path}.bak"
+    fi
+  fi
+}
+
+# Config bash preferences
+configure_bash() {
+  local bash_conf="$HOME/.bash_profile"
+  local bash_conf_local="$HOME/.bash_aliases"
+
+  backup_file "${bash_conf}"       && cp ./bash/bash_profile ${bash_conf}
+  backup_file "${bash_conf_local}" && cp ./bash/bash_profile.local ${bash_conf_local}
+}
+
+configure_checkstyle() {
+  local checkstyle_d="/usr/local/etc/checkstyle.d"
+  backup_file "${checkstyle_d}"
+  cp -r checkstyle.d ${checkstyle_d}
+
+  local java_checkstyle_script="/usr/local/bin/java-checkstyle"
+  backup_file "${java_checkstyle_script}"
+  cp ./scripts/java-checkstyle ${java_checkstyle_script}
+
+  local style_wrapper_dir="/usr/local/opt/style"
+  backup_file "${style_wrapper_dir}"
+  cp -r ./style ${style_wrapper_dir}
+
+  # Create soft links for wrapper scripts
+  ln -f -s ${style_wrapper_dir}/bin/* /usr/local/bin/
+}
+
 
 
 #### Main
@@ -101,30 +118,23 @@ install_vim_plugins() {
 install_xcode
 
 # Install `Homebrew`
-install_package brew Homebrew install_homebrew
+install_package brew "Homebrew" install_homebrew
 
 # Install `llvm`, `cmake`, `tree`, `wget`
-install_package llvm-gcc LLVM "brew install llvm"
-install_package cmake cmake   "brew install cmake"
-install_package tree tree     "brew install tree"
-install_package wget wget     "brew install wget"
+install_package llvm-gcc "LLVM tools" "brew install llvm"
+install_package cmake "cmake command" "brew install cmake"
+install_package tree "tree command"   "brew install tree"
+install_package wget "wget command"   "brew install wget"
 
-install_vim
+install_and_configure_vim
 configure_bash
-
-# Install vundle
-# git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-install_package vundle vundle "git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim"
-
-configure_vim
-install_vim_plugins # Depends on vundle
 
 # Install Java JDK 1.8, `google-java-format` requires it
 install_package javac "Java 8" "brew cask install java"
 
 # Install style checkers and formatters
-install_package clang-format clang-format                  "brew install clang-format" # C-family code style check and format
-install_package checkstyle checkstyle                      "brew install checkstyle" # Java code style check
+install_package clang-format "Clang-format"                "brew install clang-format" # C-family code style check and format
+install_package checkstyle "Checkstyle"                    "brew install checkstyle" # Java code style check
 install_package google-java-format "Google Java Formatter" "brew install google-java-format" # Java code formatter
 install_package scalastyle "Scala Style Checker"           "brew install scalastyle" # Scala code style check
 install_package scalariform "Scala Code Formatter"         "brew install scalariform" # Scala code formatter
